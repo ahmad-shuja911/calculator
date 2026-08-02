66d8344fe43e6a4d2a7596aad30b40af7c09db61
ahmad-shuja911 <ahmadshuja839@gmail.com>
2026-08-02 20:30:29 +0500

    Add initial project files including HTML, CSS, JS, settings, and README

---
+++ script.js
@@ -0,0 +1,57 @@
+const display = document.getElementById("display");
+const historyDiv = document.getElementById("history");
+
+let history = JSON.parse(localStorage.getItem("history")) || [];
+
+showHistory();
+
+function addNum(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function backspace(){
+    display.value = display.value.slice(0,-1);
+}
+
+function calculate(){
+
+    if(display.value==="") return;
+
+    try{
+
+        let expression = display.value;
+        let result = eval(expression);
+
+        history.unshift(`${expression} = ${result}`);
+
+        localStorage.setItem("history",JSON.stringify(history));
+
+        showHistory();
+
+        display.value = result;
+
+    }catch{
+        display.value = "Error";
+    }
+
+}
+
+function showHistory(){
+
+    historyDiv.innerHTML="";
+
+    history.forEach(item=>{
+
+        const div=document.createElement("div");
+        div.className="history-item";
+        div.innerText=item;
+
+        historyDiv.appendChild(div);
+
+    });
+
+}
